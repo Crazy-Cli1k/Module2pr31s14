3fae0519c6b8378f35271a72f264d87c1213f16d
310-09 <310-09@310-09>
2021-12-08 17:54:43 +0500

    Добавьте файлы проекта.

---
+++ App1/App1/Page1.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Clicked(object sender, EventArgs a)
+        {
+            App.Current.MainPage = new Page2();
+        }
+    }
+}
---
+++ App1/App1/Page2.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Button1_Clicked(object sender, EventArgs a)
+        {
+            int Object1;
+            int Object2;
+        }
+    }
+}
